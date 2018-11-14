90e9b61027e97601ba3f8aed48bbe3f10b4a4004
Adrien <chemladrien@gmail.com>
2018-11-14 16:29:25 +0100

    edit listadvert

--- listadvert.php
+++ listadvert.php
@@ -1,15 +1,16 @@
 <?php    
 // lancement de la requete
-$sql = 'SELECT title, date, number, img_advert FROM advert WHERE 1';
+$sql = 'SELECT title, date, number, img_advert, CONCAT(profil.firstname, " ",profil.lastname) autor FROM advert a INNER JOIN profil ON profil.ID = a.autor WHERE 1';
+//$sql = 'SELECT ID, firstname, lastname FROM profil LEFT JOIN advert ON profil.ID = advert.autor WHERE 1';
+//request search
 if(isset($_GET['q']) AND !empty($_GET['q'])){
     $q= htmlspecialchars($_GET['q']);
     $sql .= ' AND title LIKE "%'.$q.'%"';
 }
-
+//récupération des cartes perso pour le profil
 if (explode('/', $_SERVER['REQUEST_URI'])[2] == 'profil.php') {
-    $sql .= ' AND autor = ' . $_SESSION;
+    $sql .= ' AND a.autor = ' . $_SESSION;
 }
-
     $req = $db->prepare($sql);
     $req->execute();
 
@@ -27,6 +28,7 @@ if (explode('/', $_SERVER['REQUEST_URI'])[2] == 'profil.php') {
             <h5 class="card-title"><?php echo $results[$i]["title"]; ?></h5>
             <p class="card-text"> Date de publication : <?php echo $results[$i]["date"] ?></p>
             <p class="card-text"> Nombre de <?php echo $results[$i]["title"] . ": " . $results[$i]["number"]; ?></p>
+            <p class="card-text"> Auteur : <?php echo $results[$i]["autor"]; ?></p>
             <a href="#" class="btn btn-primary">Contacter</a>
           </div> 
         </div>
