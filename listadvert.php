8410410e3e3bfc3ce938d1982f1c0871d16e1659
Kilian <killempe@sfr.fr>
2018-11-15 16:00:15 +0100

    version final

--- listadvert.php
+++ listadvert.php
@@ -1,25 +1,39 @@
 <?php    
-// lancement de la requete
-$sql = 'SELECT title, date, number, img_advert, CONCAT(profil.firstname, " ",profil.lastname) autor FROM advert a INNER JOIN profil ON profil.ID = a.autor WHERE 1';
-//$sql = 'SELECT ID, firstname, lastname FROM profil LEFT JOIN advert ON profil.ID = advert.autor WHERE 1';
-//request search
-if(isset($_GET['q']) AND !empty($_GET['q'])){
-    $q= htmlspecialchars($_GET['q']);
-    $sql .= ' AND title LIKE "%'.$q.'%"';
-}
-//récupération des cartes perso pour le profil
-if (explode('/', $_SERVER['REQUEST_URI'])[2] == 'profil.php') {
-    $sql .= ' AND a.autor = ' . $_SESSION['ID'];
-}
-    $req = $db->prepare($sql);
-    $req->execute();
 
-    $results = $req->fetchAll(PDO::FETCH_ASSOC);
+    //verifie où on commence
+    function startsWith($string, $startString) {
+        $len = strlen($startString); 
+        return (substr($string, 0, $len) === $startString);
+    }
+
+    //lancement de la requete
+    $sql = 'SELECT title, date, number, img_advert, CONCAT(profil.firstname, " ",profil.lastname) autor FROM advert a INNER JOIN profil ON profil.ID = a.autor WHERE 1';
+
+    //request search
+    if(isset($_GET['q']) AND !empty($_GET['q'])){
+        $q= htmlspecialchars($_GET['q']);
+        $sql .= ' AND title LIKE "%'.$q.'%"';
+    }
+
+    //récupération des cartes perso pour le profil
+    if (startsWith(explode('/', $_SERVER['REQUEST_URI'])[2], 'profil.php')) {
+        $sql .= ' AND a.autor = ' . $_SESSION['ID'];
+    }
+
+        $req = $db->prepare($sql);
+        $req->execute();
+
+        $results = $req->fetchAll(PDO::FETCH_ASSOC);
+
+        //si aucun resultats
+        if(sizeof($results) === 0) {
+            echo "Désolé nous n'avons pas trouvé ce que vous cherchez";
+        }
 ?>
 <div class="row">
-<?php for ($i = 0; $i < sizeof($results); $i++)
-    {
-        $keys = array_keys($results[$i]); ?>
+    <?php for ($i = 0; $i < sizeof($results); $i++)
+        {
+            $keys = array_keys($results[$i]); ?>
     <div class="col-sm-4">
         <div class="card">
         <div class="card-img" style="background-image:url(<?php echo $results[$i]["img_advert"];?>)">
