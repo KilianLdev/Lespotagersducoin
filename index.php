dc0b9900421627600ae672db5222761cde4b82ef
Kilian <killempe@sfr.fr>
2018-11-29 17:07:38 +0100

    Design update

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ include_once "session.php";
 ?>
 <html>
   <head>
-
   <title>Les potagers du coin</title>
   <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,7 +12,7 @@ include_once "session.php";
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="public/style/style.css">
+    <link rel="stylesheet" type="text/css" href="./public/style/style.css">
     <script type="text/javascript" src="public/js/main.js"></script>
   </head>
   <body class="home">
